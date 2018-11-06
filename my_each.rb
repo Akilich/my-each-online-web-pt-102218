c2bbfaf018f28eb110e8c4298cb82899f712092d
Akili Humphrey <akilihumphrey@gmail.com>
2018-11-06 10:00:47 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,7 +1,10 @@
-def my_each # put argument(s) here
-  words = ['hi', 'hello', 'bye', 'goodbye']
-my_each(words) do |i|
-  while 
-  return i
-end
-end
+def my_each(array)
+   i = 0
+ 
+  while i < array.length
+    yield(array[i])
+    i = i + 1
+  end
+ 
+  array
+end 
